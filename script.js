9de00d32ab5243b2b1509c3a5741e6660bc61258
razlevi <razlevio.55@gmail.com>
2022-05-06 22:35:13 +0300

    script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit", (e) => {
+  const passwordOne = document.querySelector("#password");
+  const passwordTwo = document.querySelector("#verifyPassword");
+  const errorLabel = document.querySelector(".pass-error-msg");
+  if (passwordOne.value !== passwordTwo.value) {
+    e.preventDefault();
+    passwordOne.classList.add("error");
+    passwordTwo.classList.add("error");
+    errorLabel.style.display = "block";
+  }
+});
